0a4eebe1e41c25680ec7161bd1d58c13ae5f20c6
ibych <ibych@softserveinc.com>
2021-10-06 12:14:34 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link style="margin-right:30px" to="/auth">Login</router-link>
+      <router-link style="margin-right:30px" to="/register">Register</router-link>
+       <router-link style="margin-right:30px" to="/profile">Profile</router-link>
+      <!-- <router-link style="margin-right:30px" to="/card">card</router-link>
+      <router-link style="margin-right:30px" to="/SharedCalendar">SharedCalendar</router-link> -->
+      <router-link to="/">Index</router-link>
+    </div>
+    <!-- Render the content of the current page view -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-jumbotron
+      header="DIT341 Frontend"
+      lead="Welcome to your DIT341 Frontend Vue.js App"
+    >
+      <v-button class="btn_message" variant="primary" v-on:click="getMessage()"
+        >Get Message from Server</v-button
+      >
+      <p>
+        Message from the server:<br />
+        {{ message }}
+      </p>
+    </v-jumbotron>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { Api } from '@/Api'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      message: 'none'
+    }
+  },
+  methods: {
+    getMessage() {
+      Api.get('/')
+        .then((response) => {
+          this.message = response.data.message
+        })
+        .catch((error) => {
+          this.message = error
+        })
+    }
+  }
+}
+</script>
+
+<style>
+.btn_message {
+  margin-bottom: 1em;
+}
+</style>
